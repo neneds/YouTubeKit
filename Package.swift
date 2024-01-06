6fd55e8163765bbf3bd043673cab10741e4d7963
Dennis Merli <neneds@gmail.com>
2024-01-06 12:22:24 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "YouTubeKit",
+    name: "YouTubeKitNDS",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v16),
         .macOS(.v13)
     ],
     products: [
